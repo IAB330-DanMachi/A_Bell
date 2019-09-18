1368147f7322124bfe02c2a350de2b96c5364fe9
Kort <kort666666@gmail.com>
2019-09-18 16:55:19 +1000

    Student Bluetooth, Staff Profile pages add

---
+++ A_Bell/StaffProfile.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace A_Bell
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StaffProfile : ContentPage
+    {
+        public StaffProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ A_Bell/StudentBluetooth.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace A_Bell
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StudentBluetooth : ContentPage
+    {
+        public StudentBluetooth()
+        {
+            InitializeComponent();
+        }
+    }
+}
