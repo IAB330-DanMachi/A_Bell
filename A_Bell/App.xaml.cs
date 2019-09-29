c231d5037e32d6c81893a6d3cc1f9b9cb6804284
Kort <kort666666@gmail.com>
2019-09-29 21:24:23 +1000

    Update StudentBluetooth page UI design

--- A_Bell/App.xaml.cs
+++ A_Bell/App.xaml.cs
@@ -11,7 +11,7 @@ namespace A_Bell
         {
             InitializeComponent();
 
-            MainPage = new StaffProfile();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
