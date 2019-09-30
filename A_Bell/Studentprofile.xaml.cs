4316a27ac87ca25418bbaccf75d1f159e2fd5858
Kort <kort666666@gmail.com>
2019-09-30 20:34:31 +1000

    Update StudentProfile page

--- A_Bell/Studentprofile.xaml.cs
+++ A_Bell/Studentprofile.xaml.cs
@@ -7,19 +7,24 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace A_Bell.Views
+namespace A_Bell
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Studentprofile : ContentPage
+    public partial class StudentProfile : ContentPage
     {
-        public Studentprofile()
+        public StudentProfile()
         {
             InitializeComponent();
+            lblClickFucn();
         }
-        private void Button_Clicked(object sender, EventArgs e)
+        void lblClickFucn()
         {
-            submit.Text = "Teacher verification code: " + "\n";
-            submit2.Text = "New IMEI number: " + "\n";
+            lblClick.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() => {
+                    DisplayAlert("Please Enter Verfication code", "__ __ __  __ __ __", "OK");
+                })
+            });
         }
     }
 }
