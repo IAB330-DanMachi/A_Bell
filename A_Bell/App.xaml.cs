57045a810f28d12340ed229f7af6a884e8ed8785
Kort <kort666666@gmail.com>
2019-11-04 14:50:09 +1000

    Update Unlist Page

--- A_Bell/App.xaml.cs
+++ A_Bell/App.xaml.cs
@@ -11,7 +11,7 @@ namespace A_Bell
         {
             InitializeComponent();
 
-            MainPage = new Signup();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
